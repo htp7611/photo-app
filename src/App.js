c6d7eb8cec3c5beab922fbee4178f99e4d3dc334
Hung Tran Phan <hung.tranphan@orientsoftware.com>
2020-05-13 17:01:23 +0700

    feat(set-up): create component

--- src/App.js
+++ src/App.js
@@ -1,24 +1,29 @@
-import React from 'react';
-import logo from './logo.svg';
+import React, {Suspense} from 'react';
 import './App.css';
+import {BrowserRouter, Link, Switch, Redirect} from "react-router-dom";
+import NotFound from "./components/NotFound/NotFound";
+
+// lazy load module
+const Photo = React.lazy(() => import('./features/Photo/Photo'));
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div className="photo-app">
+      <Suspense fallback={<div>Loading ...</div>}>
+        <BrowserRouter>
+          <ul>
+            <li><Link to='/photos'>Go to photo pages</Link></li>
+            <li><Link to='/photos/add'>Add new photo page</Link></li>
+            <li><Link to='/photos/123'>Edit photo page</Link></li>
+          </ul>
+          <Switch>
+            <Redirect exact from='/' to='/photo' />
+
+            <Route path='/photos' component={Photo} />
+            <Route component={NotFound} />
+          </Switch>
+        </BrowserRouter>
+      </Suspense>
     </div>
   );
 }
---
+++ src/features/Photo/Photo.jsx
@@ -0,0 +1,25 @@
+import React from 'react';
+import { Route, Switch, useRouteMatch } from 'react-router-dom';
+import PropTypes from 'prop-types';
+import NotFound from "../../components/NotFound/NotFound";
+import MainPage from "./pages/Main/Main";
+import AddEditPage from "./pages/AddEdit/AddEdit";
+
+Photo.propTypes = {
+
+};
+
+function Photo(props) {
+  const match = useRouteMatch();
+  return (
+      <Switch>
+        <Route exact path={match.url} component={MainPage} />
+        <Route path={`${match.url}/add`} component={AddEditPage} />
+        <Route path={`${match.url}/:photoId`} component={AddEditPage} />
+
+        <Route component={NotFound} />
+      </Switch>
+  );
+}
+
+export default Photo;
